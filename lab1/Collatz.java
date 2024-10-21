25259b65a088c4078cc9ed1a1934837a3ef87f5e
Na Sang <cs.na.sang@gmail.com>
2024-10-20 19:05:11 -0700

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,15 +3,14 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /**
+     * Collatz sequence
+     * */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
-        } else {
-            return n * 2;
+        if (n % 2 == 1) {
+          return 3*n + 1;
         }
+        return n/2;
     }
 
     public static void main(String[] args) {
